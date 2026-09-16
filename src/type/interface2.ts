8001d0871afb71a29938d131c31cb3f9fad03712
amioswe <amioswe@gmail.com>
2026-09-16 15:06:33 +0600

    anvanced more interface with type script

---
+++ src/type/interface2.ts
@@ -0,0 +1,28 @@
+type userRoll="User" | "Modaretor" | "Guest" ;
+
+interface User{
+name:string,
+roll?:userRoll,
+email:string
+
+}
+
+interface Modarator extends User{
+name:string,
+paymentusers:string[];
+}
+ 
+
+interface Guest extends User{
+
+    guestDetais:string[];
+}
+
+const fulldetails:Modarator={
+
+name:'Amiodas',
+email:'sfdfgdgg',
+paymentusers:['dyudhsdh','jijwdjpj','jwh'];
+
+
+}
